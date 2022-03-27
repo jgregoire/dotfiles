637e68a1ad30763a4db674e507d0e4f16e0bd68e
James Gregoire <jgregoire@ieee.org>
2022-03-27 13:28:55 -0400

    Disabled autocomplete in comments.

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -32,7 +32,16 @@ cmp.setup({
         -- { name = 'snippy' }, -- For snippy users.
     }, {
         { name = 'buffer' },
-    })
+    }),
+    enabled = function() -- Disable autocomplete in comments.
+        local context = require'cmp.config.context'
+        if vim.api.nvim_get_mode().mode == 'c' then
+            return true
+        else
+            return not context.in_treesitter_capture("comment")
+            and not context.in_syntax_group("Comment")
+        end
+    end
 })
 
 -- Set configuration for specific filetype.
