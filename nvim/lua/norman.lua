24af0f486ba873fb25913789bdd51d5dbfba6e22
James Gregoire <jgregoire@ieee.org>
2022-04-15 10:38:33 -0400

    Further nvim Norman customization

--- nvim/lua/norman.lua
+++ nvim/lua/norman.lua
@@ -6,11 +6,36 @@ local b = vim.bo
 
 local map = vim.api.nvim_set_keymap
 
-options = { noremap = true }
+local options = { noremap = true }
 
--- Remap nioh commands
--- s for insert
-map('n', 's', 'i', options)
+-- Insert = s
+map('n', 's', 'i', options) -- Insert before cursor.
+map('n', 'S', 'I', options) -- Insert at beginning of line.
+
+-- Z, X, C, V for Undo, Cut, Copy, Paste
+map('n', 'z',  'u',     options) -- Undo.
+map('n', 'y',  '<C-r>', options) -- Redo.
+map('n', 'x',  'd',     options) -- Cut/delete.
+map('n', 'xx', 'dd',    options) -- Cut line.
+map('n', 'X',  'd$',    options) -- Cut to end of line
+map('n', 'c',  'y',     options) -- Copy/yank.
+map('n', 'cc', 'yy',    options) -- Copy line.
+map('n', 'C',  'y$',    options) -- Copy to end of line.
+map('n', 'v',  'p',     options) -- Paste before cursor.
+
+-- Change
+map('n', 'g',  'c',     options) -- Change.
+map('n', 'gg', 'cc',    options) -- Change entire line.
+map('n', 'G',  'C',     options) -- Change to end of line.
+
+-- Substitute
+map('n', 'e',  's',     options) -- Substitute character.
+map('n', 'E',  'S',     options) -- Substitute line.
+
+-- Navigation
+map('n', 'p',  '%',     options) -- Jump to matching paren.
+map('n', 'vv', 'zz',    options) -- Center screen on cursor.
 
 -- Map hjkl to nioh
-o.langmap = 'nh,ik,oj,hl'
+o.langmap = 'nh,ik,oj,hl' -- Note up/down are flipped from standard.
+
