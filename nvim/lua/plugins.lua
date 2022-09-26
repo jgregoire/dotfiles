dd2de9dd67265ebcffe7142d7a6aefe429498073
james <jgregoire@tutamail.com>
2022-09-26 10:38:05 -0400

    nvim plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -41,7 +41,9 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true },
         config = function()
-            require('lualine').setup()
+            require('lualine').setup({
+                options = { theme = 'auto' },
+            })
         end,
     }
 
