91748030046b025c8bc4a0e94ece6d758db84858
james <jgregoire@tutamail.com>
2022-11-06 15:47:42 -0500

    Tweaking shortcuts

--- nvim/lua/legendary_config.lua
+++ nvim/lua/legendary_config.lua
@@ -1,7 +1,7 @@
 -- Setup for legendary.nvim plugin and Norman keymap.
 
 vim.g.mapleader = ','
-vim.o.langmap = 'nh,ik,oj,hl'
+--vim.o.langmap = 'nh,ik,oj,hl'
 
 local std_opts = { noremap = true }
 
@@ -15,14 +15,24 @@ require('legendary').setup({
 
     keymaps = {
         -- Modes
-        { 'jk',     '<ESC>', description = 'Back to Normal mode', mode = { 'i' } },
-        { 's',      'i',     description = 'Insert before cursor', opts = {}, mode = { 'n' } },
-        { 'S',      'I',     description = 'Insert at beginning of line', opts = {}, mode = { 'n' } },
-        { 'a',               description = 'Append after cursor' },
-        { 'A',               description = 'Append at end of line' },
-        { '<C-b>',  '<C-v>', description = 'Visual Block mode', mode = { 'n' } },
-        { '<C-v>',  'v',     description = 'Visual Char mode', mode = { 'n' } },
-        { '<C-l>',  'V',     description = 'Visual Line mode', mode = { 'n' } },
+        { 'jk',    '<ESC>', mode = { 'i' },      description = 'Back to Normal mode' },
+        { 's',     'i',     mode = { 'n' },      description = 'Insert before cursor' },
+        { 'S',     'I',     mode = { 'n' },      description = 'Insert at beginning of line' },
+        { 'a',              mode = { 'n' },      description = 'Append after cursor' },
+        { 'A',              mode = { 'n' },      description = 'Append at end of line' },
+        { '<C-b>', '<C-v>', mode = { 'n', 'i' }, description = 'Visual Block mode' },
+        { '<C-v>', 'v',     mode = { 'n', 'i' }, description = 'Visual Char mode' },
+        { '<C-l>', 'V',     mode = { 'n', 'i' }, description = 'Visual Line mode' },
+
+        -- Arrows
+        { 'n', 'h', mode = { 'n', 'v' }, description = 'Left' },
+        { 'N', 'h', mode = { 'n', 'v' }, description = 'Left' },
+        { 'i', 'k', mode = { 'n', 'v' }, description = 'Up' },
+        { 'I', 'k', mode = { 'n', 'v',}, description = 'Up' },
+        { 'o', 'j', mode = { 'n', 'v' }, description = 'Down' },
+        { 'O', 'j', mode = { 'n', 'v' }, description = 'Down' },
+        { 'h', 'l', mode = { 'n', 'v' }, description = 'Right' },
+        { 'H', 'l', mode = { 'n', 'v' }, description = 'Right' },
 
         -- Editing
         { 'z',  'u',     mode = { 'n' }, description = 'Undo' },
@@ -41,7 +51,11 @@ require('legendary').setup({
         { 'e',  's',     mode = { 'n' }, description = 'Substitute character' },
         { 'E',  'S',     mode = { 'n' }, description = 'Substitute line' },
 
-        -- Navigation
+        -- Prepositions
+        { 'i',  'i',     mode = { 'o' }, description = 'Inside' },
+        { 'a',  'a',     mode = { 'o' }, description = 'Around' },
+
+        -- Motions
         { 'p',  '%',     mode = { 'n' }, description = 'Jump to matching paren' },
         { 'vv', 'zz',    mode = { 'n' }, description = 'Center screen on cursor' },
         { 'f',  'b',     mode = { 'n' }, description = 'Previous word' },
