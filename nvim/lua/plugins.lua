d5ec67045f6a73524b57e5fe9d8c319ed95996ff
James Gregoire <jgregoire@ieee.org>
2023-10-17 11:01:37 -0400

    Added csv mode plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -15,6 +15,27 @@ return require('packer').startup(function(use)
         end,
     }
 
+    -- CSV mode
+    use {
+        'cameron-wags/rainbow_csv.nvim',
+        config = function()
+            require 'rainbow_csv'.setup(){}
+        end,
+        module = {
+            'rainbow_csv',
+            'rainbow_csv.fns',
+        },
+        ft = {
+            'csv',
+            'tsv',
+            'csv_semicolon',
+            'csv_whitespace',
+            'csv_pipe',
+            'rfc_csv',
+            'rfc_semicolon',
+        }
+    }
+
     -- Diff/merge tool
     --[[use {
         'sindrets/diffview.nvim',
