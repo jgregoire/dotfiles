a9aac575ace3807515eaf492e2aa49c03b564f65
james <jgregoire@tutamail.com>
2024-01-17 21:46:12 -0500

    wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,10 +6,30 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
-config.color_scheme = 'onedark_vivid'
+-- Appearance
+config.color_scheme = 'onedarkpro_onedark_vivid'
 config.bold_brightens_ansi_colors = false
-config.font = wezterm.font('FiraCode Nerd Font')
-config.font_size = 12.0
+-- config.enable_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.75
+config.window_padding = {
+    left = 0,
+    right = '1cell',
+    top = 0,
+    bottom = 0,
+}
+
+local dir_char = package.config:sub(1,1)
+-- Linux-specific options
+if dir_char == '/' then
+    config.font = wezterm.font('Fira Code')
+    config.font_size = 14.0
+-- Windows-specific options
+elseif dir_char == '\\' then
+    config.font = wezterm.font('FiraCode Nerd Font')
+    config.font_size = 12.0
+    config.allow_win32_input_mode = true
+end
 
 return config
 
