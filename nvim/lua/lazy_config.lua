dea6f075a9483537e268d09befef87f32b7d51ee
James Gregoire <jgregoire@ieee.org>
2024-03-04 11:54:36 -0500

    Removed auto-session

--- nvim/lua/lazy_config.lua
+++ nvim/lua/lazy_config.lua
@@ -55,21 +55,6 @@ require('lazy').setup({ -- Plugins
         end,
     },
     {
-        'rmagatti/auto-session',
-        config = function ()
-            require('auto-session').setup({
-                log_level = 'error',
-                auto_session_enable_last_session = false,
-                auto_session_root_dir = vim.fn.stdpath('data') .. '/sessions/',
-                auto_session_enabled = true,
-                auto_session_suppress_dirs = nil,
-                auto_session_use_git_branch = nil,
-                auto_session_last_session_dir = '',
-                bypass_session_save_file_types = nil,
-            })
-        end
-    },
-    {
         'williamboman/mason.nvim',
         config = function ()
             require('mason').setup({
