c12b56816ce866b02723845693db14eb5357a02e
James Gregoire <jgregoire@ieee.org>
2024-03-29 09:12:54 -0400

    beginning to add plugins

---
+++ nvim/lua/plugins/15_cmp.lua
@@ -0,0 +1,109 @@
+require('cmp-natdat').setup()
+
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+local lspkind = require('lspkind')
+
+local has_words_before = function ()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line-1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+---@diagnostic disable-next-line:redundant-parameter
+cmp.setup({
+    formatting = {
+        format = lspkind.cmp_format({
+            mode = 'symbol_text',
+            maxwidth = 50,
+            ellipsis_char = '...',
+            show_labelDetails = true,
+        })
+    },
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        -- Invoke completion
+        ['<C-Space>'] = cmp.mapping.complete(),
+
+        -- Abort completion
+        ['<C-c>'] = cmp.mapping.abort(),
+
+        --Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items. 
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+
+        -- Select next suggestion.
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_locally_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+
+        -- Select previous suggestion.
+        ['<C-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+        { name = 'luasnip' },
+        { name = 'neorg' },
+        { name = 'calc' },
+        { name = 'natdat' },
+    }, {
+        { name = 'buffer' },
+    }),
+    enabled = function () -- Disable autocomplete in comments.
+        local context = require('cmp.config.context')
+        if vim.api.nvim_get_mode().mode == 'c' then
+            return false -- Default is true, but this conflicts with Noice.
+        else
+            return not context.in_treesitter_capture('comment')
+            and not context.in_syntax_group('Comment')
+        end
+    end
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+        { name = 'git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+        { name = 'buffer' },
+    })
+})
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    })
+})
+
+-- cmp autopairs support
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
