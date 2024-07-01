0305166615230a12f6630a0e9f6216978b8b5b8c
James Gregoire <jgregoire@ieee.org>
2024-07-01 13:23:32 -0400

    added lualine plugin + config

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,63 @@
+require('lualine').setup({
+    options = {
+        theme = 'auto',
+        -- theme = 'gruvbox-material',
+        component_separators = {
+            left  = '│',
+            right = '│'
+        },
+        section_separators = {
+            left  = '',
+            right = ''
+        },
+    },
+    extensions = {
+        'toggleterm',
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = {
+            'branch',
+            {
+                'diff',
+                symbols = {
+                    added    = ' ',
+                    modified = ' ',
+                    removed  = ' ',
+                },
+            },
+            {
+                'diagnostics',
+                symbols = {
+                    error = ' ',
+                    warn  = ' ',
+                    info  = ' ',
+                    hint  = ' ',
+                },
+            },
+        },
+        lualine_c = {
+            {
+                'buffers',
+                mode = 2, -- show name and index
+                symbols = {
+                    modified = ' '
+                },
+            },
+        },
+        lualine_x = {
+            'encoding',
+            'fileformat',
+            'filetype' },
+            lualine_y = { 'progress', },
+            lualine_z = { 'location', },
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { 'filename' },
+            lualine_x = { 'location' },
+            lualine_y = {},
+            lualine_z = {}
+        },
+    })
